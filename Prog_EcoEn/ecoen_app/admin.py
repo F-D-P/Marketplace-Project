c4290ed440db0abdb32cfc885a1343ed5b1f0b3b
Franco <francoportillocpm@gmail.com>
2025-11-24 23:57:18 -0300

    Primer commit del proyecto EcoEn

--- Prog_EcoEn/ecoen_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Producto, Perfil, Opinion, Compra, Puntuacion
-
-admin.site.register(Producto)
-admin.site.register(Perfil)
-admin.site.register(Opinion)
-admin.site.register(Compra)
-admin.site.register(Puntuacion)
--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import chat_page, chatbot_response
-
-urlpatterns = [
-    path('', chat_page, name='chat_page'),  # interfaz del chat
-    path('respond/', chatbot_response, name='chatbot_response'),  # endpoint AJAX
-]
